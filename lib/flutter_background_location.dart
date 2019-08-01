869e30cedabffd9a7021890937ffba4f9690278a
shahxadakram <shahxadakram@gmail.com>
2019-08-01 13:36:56 +0500

    Package name Changed

---
+++ lib/flutter_background_location.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class FlutterBackgroundLocation {
+  static const MethodChannel _channel =
+  const MethodChannel('flutter_background_location');
+
+  static stopLocationService() {
+    _channel.invokeMapMethod("stop_location_service");
+  }
+
+  static startLocationService() {
+    _channel.invokeMapMethod("start_location_service");
+  }
+
+  Future<_Location> getCurrentLocation() async {
+    Completer<_Location> completer = Completer();
+
+    _Location _location = _Location();
+    await getLocationUpdates((location) {
+      _location.latitude = location.latitude;
+      _location.longitude = location.longitude;
+      _location.accuracy = location.accuracy;
+      _location.altitude = location.altitude;
+      _location.bearing = location.bearing;
+      completer.complete(_location);
+    });
+    return completer.future;
+  }
+
+  static getLocationUpdates(Function(_Location) location) {
+    _channel.setMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == "location") {
+        Map locationData = Map.from(methodCall.arguments);
+        location(_Location(
+            latitude: locationData["latitude"],
+            longitude: locationData["longitude"],
+            altitude: locationData["altitude"],
+            accuracy: locationData["accuracy"],
+            bearing: locationData["bearing"]));
+      }
+    });
+  }
+}
+
+class _Location {
+  _Location(
+      {this.longitude,
+        this.latitude,
+        this.altitude,
+        this.accuracy,
+        this.bearing});
+
+  double latitude;
+  double longitude;
+  double altitude;
+  double bearing;
+  double accuracy;
+}
